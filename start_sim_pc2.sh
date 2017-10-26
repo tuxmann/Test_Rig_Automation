9589f894888072e866a751be1e4e13059be9205f
Jason Mann <aztuxmann+github@gmail.com>
2017-10-26 15:48:47 -0700

    Leaned out final version.

--- start_sim_pc2.sh
+++ start_sim_pc2.sh
@@ -1,55 +1,29 @@
 #!/bin/sh
-# Request a filename and mode from the user. Enter filename, select mode, 
-# and press "Start Simulation"
-# 
-# Move the Data acquisition window to the top right of the screen.
+# Enter the filename, and mode received from the test_supervisor script.
+# Start simulation.
 # xdotool getmouselocation --shell
-# X,Y relative to window 0,0
-# filename field: X=580	Y=231
-# Mode menu:	  X=580	Y=175
-# Start Sim:	  X=580	Y=353
 
-enter_data(){
-read -p "Enter the filename for this test: " filename
-	until [ "$mode" == "1" ] || [ "$mode" == "2" ]; do
-		read -p "Enter (1)Normal or (2)Direct: " mode
-	done	
-#else echo "number did match"
-}
-
-echo $'\n' "	Start Sim PC #2 Script started" $'\n'
-
-enter_data
+filename="$1"
+mode="$2"
+delay=0.2
 
 # Move mouse to 0 0 of the Data acquisition window.
 wid=`xdotool search --name "acquisition"`
-xdotool windowactivate $wid mousemove --sync --window $wid 0 0; sleep 0.2
+xdotool windowactivate $wid mousemove --sync --window $wid 0 0; sleep $delay
 
 # Enter the file name
-xdotool mousemove_relative --sync 580 180 click --repeat 3 --delay 40 1; sleep 0.2
+xdotool mousemove_relative --sync 580 180 click --repeat 3 --delay 40 1; sleep $delay
 xdotool key "BackSpace"
-xdotool type --clearmodifiers $filename; sleep 0.2
+xdotool type --clearmodifiers $filename; sleep $delay
 
 # Select Normal or Direct Mode
-xdotool mousemove_relative --sync 0 -55 click 1; sleep 0.1
+xdotool mousemove_relative --sync 0 -55 click 1; sleep $delay
 if [ "$mode" = "1" ] ; then
-	xdotool key "Up"; sleep 0.1
+	xdotool key "Up"; sleep $delay
 else
-	xdotool key "Down"; sleep 0.1
+	xdotool key "Down"; sleep $delay
 fi
 xdotool key "Return"
 
 # Start Simulation
 xdotool mousemove_relative --sync 0 180 click 1
-echo "	Job Complete!" $'\n'
-
-#####################################################################
-######################   LUMBER YARD    #############################
-#####################################################################
-#echo "You typed:" $filename $mode
-
-#echo "	1"
-#echo "	2"
-#echo "	3"
-
-
