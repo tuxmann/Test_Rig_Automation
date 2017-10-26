8316b5dcb5fd0396afa39c6a13f40cc343f5ee15
Jason Mann <aztuxmann+github@gmail.com>
2017-10-26 15:51:46 -0700

    Leaned out final version

--- start_sim_pc5.sh
+++ start_sim_pc5.sh
@@ -1,55 +1,34 @@
 #!/bin/sh
-# Request a filename and mode from the user. Enter filename, select mode, 
-# and press "Start Simulation"
-# 
-# Move the Data acquisition window to the top right of the screen.
+# Enter the filename, and mode received from the test_supervisor script.
+# Start simulation.
 # xdotool getmouselocation --shell
-# X,Y relative to window 0,0
-# filename field: X=1340	Y=120
-# Mode menu:	  X=580	Y=470
-# Start Sim:	  X=580	Y=60
 
-enter_data(){
-read -p "Enter the filename for this test: " filename
-	until [ "$mode" == "1" ] || [ "$mode" == "2" ]; do
-		read -p "Enter (1)Normal or (2)Direct: " mode
-	done	
-#else echo "number did match"
-}
+exec 3>&1 1>/dev/null 2>&1	# Silence terminal output so "Defaulting to search window name" Doesn't appear
+filename="$1"
+mode="$2"
+delay=0.2
+LD_LIBRARY_PATH=/usr/local/lib
+export LD_LIBRARY_PATH
 
-echo $'\n' "	Start Sim PC #2 Script started" $'\n'
-
-enter_data
+#enter_data
 
 # Move mouse to 0 0 of the Data acquisition window.
-wid=`xdotool search --name "acquistion"`	# spelled incorrectly on purpose.
-xdotool windowactivate $wid mousemove --sync --window $wid 0 0; sleep 2
+wid=`xdotool search --desktop 0 "acquistion"`	# spelled incorrectly on purpose.
+xdotool windowactivate $wid mousemove --sync --window $wid 0 0; sleep $delay
 
-# Enter the file name
-xdotool mousemove_relative --sync 1340 120 click --repeat 3 --delay 40 1; sleep 2
+# Click 3 times to highlight all text, press BackSpace, Enter the filename
+xdotool mousemove_relative --sync 1340 120 click --repeat 3 --delay 40 1; sleep $delay
 xdotool key "BackSpace"
-xdotool type --clearmodifiers $filename; sleep 2
+xdotool type --clearmodifiers $filename; sleep $delay
 
 # Select Normal or Direct Mode
-xdotool mousemove_relative --sync 0 350 click 1; sleep 1
+xdotool mousemove_relative --sync 0 350 click 1; sleep $delay
 if [ "$mode" = "1" ] ; then
-	xdotool key "Up"; sleep 1
+	xdotool key "Up"; sleep $delay
 else
-	xdotool key "Down"; sleep 1
+	xdotool key "Down"; sleep $delay
 fi
 xdotool key "Return"
 
 # Start Simulation
 xdotool mousemove_relative --sync 0 -410 click 1
-echo "	Job Complete!" $'\n'
-
-#####################################################################
-######################   LUMBER YARD    #############################
-#####################################################################
-#echo "You typed:" $filename $mode
-
-#echo "	1"
-#echo "	2"
-#echo "	3"
-
-
