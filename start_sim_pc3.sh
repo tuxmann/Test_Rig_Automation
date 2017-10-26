74238aa30840f179bd2941e8712c033fb32fa2da
Jason Mann <aztuxmann+github@gmail.com>
2017-10-26 15:50:14 -0700

    Leaned out final version

--- start_sim_pc3.sh
+++ start_sim_pc3.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# Request a filename and mode from the user. Enter filename, select mode, 
-# and press "Start Simulation"
+# Enter the filename, and mode received from the test_supervisor script.
+# Start simulation.
+# xdotool getmouselocation --shell
 # 
 # IMPORTANT NOTE!!! PLEASE READ!!!
 # NOTE: xdotool DOES NOT MOVE THE WINDOW OFF THE SCREEN CORRECTLY.
@@ -9,56 +10,30 @@
 #       SCROLLBAR IS VISIBLE. THE RESULT WILL BE THAT THE FULL BUTTONS
 #       AND SELECTION MENUS ON THE RIGHT SIDE ARE VISIBLE.
 #
-#
-# Move the Data acquisition window to the top right of the screen.
-# xdotool getmouselocation --shell
-# X,Y not relative 
-# filename field: X=1230  Y=405
-# Mode menu:	  X=1230  Y=325
-# Start Sim:	  X=1230  Y=610
+#	Try to fix this moved window by checking position.
 
-enter_data(){
-read -p "Enter the filename for this test: " filename
-	until [ "$mode" == "1" ] || [ "$mode" == "2" ]; do
-		read -p "Enter (1)Normal or (2)Direct: " mode
-	done	
-#else echo "number did match"
-}
 
-echo $'\n' "	Start Sim PC #2 Script started" $'\n'
+filename="$1"
+mode="$2"
+delay=0.2
 
-enter_data
-
-# Move mouse to 0 0 of the Data acquisition window.
+# Activate the Data acquisition window. Mouse is not moved to top left of window.
 wid=`xdotool search --name "acquisition"`
-xdotool windowactivate $wid #mousemove --sync --window $wid 0 0; 
-sleep 2
+xdotool windowactivate $wid;sleep $delay
 
 # Enter the file name
-xdotool mousemove --sync 1230 405 click --repeat 3 --delay 40 1; sleep 2
+xdotool mousemove --sync 1230 405 click --repeat 3 --delay 40 1; sleep $delay
 xdotool key "BackSpace"
-xdotool type --clearmodifiers $filename; sleep 2
+xdotool type --clearmodifiers $filename; sleep $delay
 
 # Select Normal or Direct Mode
-xdotool mousemove --sync 1230 325 click 1; sleep 1
+xdotool mousemove --sync 1230 325 click 1; sleep $delay
 if [ "$mode" = "1" ] ; then
-	xdotool key "Up"; sleep 1
+	xdotool key "Up"; sleep $delay
 else
-	xdotool key "Down"; sleep 1
+	xdotool key "Down"; sleep $delay
 fi
 xdotool key "Return"
 
 # Start Simulation
 xdotool mousemove --sync 1230 610 click 1
-echo "	Job Complete!" $'\n'
-
-#####################################################################
-######################   LUMBER YARD    #############################
-#####################################################################
-#echo "You typed:" $filename $mode
-
-#echo "	1"
-#echo "	2"
-#echo "	3"
-
-
