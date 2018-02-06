213a82df34c05ade837181d49c073dd57b191e53
Jason Mann <aztuxmann+github@gmail.com>
2018-02-06 10:36:32 -0700

    Added Color and open qrpt file
    Added colored background to make it easier to see pass and fail. Added the ability to open the qrpt file after post processing is complete.

--- finish_and_report_pc2.sh
+++ finish_and_report_pc2.sh
@@ -18,11 +18,12 @@ xdotool mousemove_relative --sync 0 -100 click 1; sleep $delay
 
 xdotool mousemove --sync --window $wid 0 0; sleep $delay
 # Check if the faults are occurred
-color=`grabc & xdotool mousemove_relative --sync -- -190 -310 click 1`
+
+color=`grabc & xdotool mousemove_relative --sync 390 45 click 1`
 if [ "$color" = "#00ff00" ] ; then
-	echo "		PC #2: PASS" >&3		# ">&3 sends output to terminal
+	echo -e "		\e[102mPC #2: PASS\e[0m" >&3		# ">&3 sends output to terminal
 else
-	echo "	FAILED ###PC2### FAILED"  >&3
+	echo -e "	\e[41mPC #2: !!! FAILED !!!\e[0m"  >&3
 fi
 
 # Switch to Post Processing Window
@@ -44,4 +45,19 @@ done
 xdotool key "Return"; sleep $delay
 
 # Click Process button
-xdotool mousemove_relative --sync -- -260 325 click 1; sleep $delay
+xdotool mousemove_relative --sync -- -295 325 click 1; sleep $delay
+button_old=`grabc & xdotool mousemove_relative --sync 354 -185 click 1`
+button_new=$button_old
+xdotool click 1; sleep $delay
+until [ "$button_old" != "$button_new" ]; do
+	button_new=`grabc & sleep 0.1 && xdotool click 1`; sleep 1
+done
+
+# Open file browser and go to most recent report
+nautilus /home/data; sleep 2
+xdotool key "Down"; xdotool key "Down"; sleep 1
+xdotool key "Return"; sleep 1
+xdotool keydown "ctrl"; xdotool key "End"; xdotool keyup "ctrl"
+
+echo "	PC #2 QRPT file created"  >&3
+
