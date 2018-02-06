9466ab788bda071e3333cccabce1cd8bbed8e703
Jason Mann <aztuxmann+github@gmail.com>
2018-02-06 10:38:06 -0700

    Added Color and open qrpt file
    Added colored background to make it easier to see pass and fail. Added the ability to open the qrpt file after post processing is complete.

--- finish_and_report_pc5.sh
+++ finish_and_report_pc5.sh
@@ -3,13 +3,14 @@
 # occurred, and report faults. Activate QTDPP program and start post
 # processing. 
 
+exec 3>&1 1>/dev/null 2>&1	# Silence terminal output so "Defaulting to search window name" Doesn't appear
 LD_LIBRARY_PATH=/usr/local/lib
 export LD_LIBRARY_PATH
 delay=0.5
 fail=0
 
 # Status Box coordinates
-BOX[1]="-900 310"
+BOX[1]="440 480"
 BOX[2]="600 0"
 
 # Move mouse to 0 0 of the Data acquisition window.
@@ -26,7 +27,7 @@ xdotool mousemove_relative --sync 0 -160 click 1; sleep $delay
 xdotool mousemove --sync --window $wid 0 0; sleep $delay
 
 for i in `seq 1 2`; do 
-	color=`grabc & xdotool mousemove_relative --sync -- ${BOX[$i]} click 1`
+	color=`grabc & sleep 0.1 && xdotool mousemove_relative --sync -- ${BOX[$i]} click 1`
 	if [ "$color" = "#00ff00" ] ; then
 		sleep 0
 	else
@@ -36,9 +37,9 @@ for i in `seq 1 2`; do
 done   
 
 if [ "$fail" = 0 ] ; then
-	echo "		PC #3: PASS" >&3
+	echo -e "		\e[102mPC #5: PASS\e[0m" >&3
 else
-	echo "	FAILED ###PC5### FAILED" >&3
+	echo -e "	\e[41mFAILED ###PC5### FAILED\e[0m" >&3
 fi
 
 # Switch to Post Processing Window
@@ -59,8 +60,39 @@ for i in `seq 1 1`; do # Press the down arrow 						two times
 done   
 xdotool key "Return"; sleep $delay
 
-# Click Process button
-xdotool mousemove_relative --sync -- -260 325 click 1; sleep $delay
+process(){ # Click Process button & Check Done
+xdotool mousemove_relative --sync -- -295 325 click 1
+button_old=`grabc & sleep 0.1 && xdotool mousemove_relative --sync 354 -185 click 1`
+button_new=$button_old
+xdotool click 1; sleep $delay
+until [ "$button_old" != "$button_new" ]; do
+	button_new=`grabc & sleep 0.1 && xdotool click 1`; sleep 1
+done
+}
 
-echo "	Job Complete!"
+process
+
+sleep 2
+
+# Select most recent data file
+xdotool windowactivate $wid mousemove --sync --window $wid 0 0; sleep $delay
+xdotool mousemove_relative --sync 580 190 click 1; sleep $delay
+
+for i in `seq 1 3`; do # Press the down arrow 						three times
+	sleep $delay; xdotool key "Down" 
+done   
+
+xdotool key "Return"; sleep $delay
+
+# Click Process button
+process
+
+# Open file browser and go to most recent report
+nautilus /home/data; sleep 2
+xdotool key "Down"; xdotool key "Down"; sleep 1
+xdotool keydown "shift"; xdotool key "Down"; xdotool keyup "shift"
+xdotool key "Return"; sleep 1
+xdotool keydown "ctrl"; xdotool key "End"; xdotool keyup "ctrl"
+
+echo "	PC #5 QRPT file created"  >&3
 
